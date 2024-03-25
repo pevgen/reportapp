4b5862a08f34787b003eba1748f4a12fe60811ff
pevgen <pevgen@gmail.com>
2024-03-25 11:43:58 +0300

    Added auto reload (in developement) by spring boot devtools

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-devtools")
     implementation("org.liquibase:liquibase-core") {
         exclude(group  = "commons-logging", module = "commons-logging")
     }
